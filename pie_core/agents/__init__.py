229cfbb0993c7661444e47590b81f30e4d83b306
Eduardo Moraes <199427299+edu-moraess@users.noreply.github.com>
2026-07-23 09:09:22 -0300

    Criar pie_core/agents/__init__.py

---
+++ pie_core/agents/__init__.py
@@ -0,0 +1,13 @@
+from pie_core.agents.intent_analysis import IntentAnalysisAgent
+from pie_core.agents.memory_learning import MemoryLearningAgent
+from pie_core.agents.prompt_architect import PromptArchitectAgent
+from pie_core.agents.prompt_critic import PromptCriticAgent
+from pie_core.agents.prompt_optimizer import PromptOptimizationAgent
+
+__all__ = [
+    "IntentAnalysisAgent",
+    "PromptArchitectAgent",
+    "PromptCriticAgent",
+    "PromptOptimizationAgent",
+    "MemoryLearningAgent",
+]
